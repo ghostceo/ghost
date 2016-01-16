1c9f9a168b1c157497a03b5038e322a864a6b2a9
sevenor <1152417913@qq.com>
2016-01-16 12:40:02 +0800

    vs

---
+++ ghost.h
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include "hello1.h"
+#include "study.h"
+#include "learn.h"
+#include <iostream>
+#include <cstdio>
+using namespace std;
---
+++ ghost/ghost/ghost.cpp
@@ -0,0 +1,10 @@
+#include "ghost.h"
+using namespace std;
+int main()
+{
+	cout << "hello world!" << endl;
+
+	cin.get();//这个可以不要
+
+	return 0;
+}

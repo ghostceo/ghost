a43dcc3e6b6c0088f25daf1d6cfa47c38b7d4b07
sevenor <1152417913@qq.com>
2016-03-17 20:09:18 +0800

    lua and html&php

---
+++ lua/hello.lua
@@ -0,0 +1,31 @@
+print("hello world1")
+print(type("Hello world"))      --> string
+print(type(10.4*3))             --> number
+print(type(print))              --> function
+print(type(type))               --> function
+print(type(true))               --> boolean
+print(type(nil))                --> nil
+print(type(type(X)))            --> string
+-- function_test.lua 脚本文件
+function factorial1(n)
+    if n == 0 then
+        return 1
+    else
+        return n * factorial1(n - 1)
+    end
+end
+print(factorial1(5))
+factorial2 = factorial1
+print(factorial2(5))
+
+
+-- function_test2.lua 脚本文件
+function anonymous(tab, fun)
+    for k, v in pairs(tab) do
+        print(fun(k, v))
+    end
+end
+tab = { key1 = "val1", key2 = "val2" }
+anonymous(tab, function(key, val)
+    return key .. " = " .. val
+end)

ee7d218f705705096ecd70e817b9b1a9be01f313
sevenor <1152417913@qq.com>
2016-03-28 14:37:46 +0800

    增加#过滤

--- script/auto/auto.sh
+++ script/auto/auto.sh
@@ -16,16 +16,39 @@ if [ ! -e $serverlist_file ] ; then
     exit 0
 fi
 
-while read line
+#for ip in `awk '{print $1}' $serverlist_file` 这个过滤不了#
+for ip in `cat $serverlist_file | grep -v ^# | awk '{print $1}'`
 do
-    #echo $line
-    if [ -n "$line" ] ; then
-        echo "DOING--->>>>>" $line "<<<<<<<"
-        ssh $line $cmd_str < /dev/null > /dev/null
-        if [ $? -eq 0 ] ; then
-            echo "$cmd_str done!"
-        else
-            echo "error: " $?
-        fi
+    port=`awk -v I="$i" '{if(I==$1)print $2}' $serverlist_file`
+    user=`awk -v I="$i" '{if(I==$1)print $3}' $serverlist_file`
+    pass=`awk -v I="$i" '{if(I==$1)print $4}' $serverlist_file`
+    
+    #num1
+    #expect /root/data/auto/auto.exp $ip $port $user $pass $cmd_str
+    
+    #num2
+    echo "DOING--->>>>>" $ip "<<<<<<<"
+    ssh $ip $cmd_str < /dev/null > /dev/null
+    if [ $? -eq 0 ] ; then
+        echo "$cmd_str done!"
+    else
+        echo "error: " $?
     fi
-done < $serverlist_file
+    #
+
+done
+
+# num3
+# while read line
+# do
+#     #echo $line
+#     if [ -n "$line" ] ; then
+#         echo "DOING--->>>>>" $line "<<<<<<<"
+#         ssh $line $cmd_str < /dev/null > /dev/null
+#         if [ $? -eq 0 ] ; then
+#             echo "$cmd_str done!"
+#         else
+#             echo "error: " $?
+#         fi
+#     fi
+# done < $serverlist_file

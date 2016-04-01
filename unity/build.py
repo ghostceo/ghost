9d5971cd65625e37c7a8b94a94b86639577c56bc
sevenor <1152417913@qq.com>
2016-04-01 17:45:11 +0800

    脚本

---
+++ unity/build.py
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#参数判断  
+if [ $# != 1 ];then  
+    echo "需要一个参数。 参数是游戏包的名子"  
+    exit     
+fi  
+
+
+#UNITY程序的路径#
+UNITY_PATH=/Applications/Unity/Unity.app/Contents/MacOS/Unity
+
+#游戏程序路径#
+PROJECT_PATH=/Users/MOMO/commond
+
+#在Unity中构建apk#
+$UNITY_PATH -projectPath $PROJECT_PATH -executeMethod ProjectBuild.BuildForAndroid project-$1 -quit
+
+
+echo "Apk生成完毕"

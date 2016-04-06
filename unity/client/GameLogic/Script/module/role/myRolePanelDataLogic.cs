5871b935bb641c11efc090f765b87dae7f07651d
ghostceo <1026604862@qq.com>
2016-04-06 23:16:06 +0800

    five

---
+++ unity/client/GameLogic/Script/module/role/myRolePanelDataLogic.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myRolePanelDataLogic : IMessageHandler
+{
+    private static myRolePanelDataLogic instance;
+    private GameObject myRolePanel;
+ 
+    private myRolePanelDataLogic()
+    {
+      
+    }
+    public static myRolePanelDataLogic ins
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = new myRolePanelDataLogic();
+            }
+            return instance;
+        }
+    }
+    public void Init(GameObject _createRoleObject)
+    {
+        myRolePanel = _createRoleObject;
+        GameNetManager.ins.AddMessageHandler(Commands.ROLE_ADD, this);
+    }
+
+    public void HandleMessage(IncommingBase message)
+    {
+        switch (message.protocol)
+        {
+            case Commands.ROLE_ADD:
+                HandleCreateRole(message);
+                break;
+        }
+    }
+    public void SendCreateRole(string roleName , int career)
+    {
+        
+    }
+    public void HandleCreateRole(IncommingBase message)
+    {
+
+    }
+}

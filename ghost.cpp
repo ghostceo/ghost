810c11813a9ec69150401a5427390358618a4fbb
sevenor <1152417913@qq.com>
2016-07-26 15:58:19 +0800

    增加输入demo

--- ghost.cpp
+++ ghost.cpp
@@ -7,6 +7,11 @@ int main(int argc, char* argv[])
     // cout << argc << endl;
     // cout << argv[0] << endl;
     // cout << argv[1] << endl;
+
+    // string mystr;
+    // cout << "What's your name? ";
+    // getline (cin, mystr);
+    // cout << "Hello " << mystr << ".\n";
     
 	//struct game *g = new game();
 	//int a=4;

59829b1062679d9162d91c9e33964536c1428b87
ghostceo <1026604862@qq.com>
2016-11-18 01:16:02 +0800

    add sock demo

--- ghost.cpp
+++ ghost.cpp
@@ -115,7 +115,7 @@ int main(int argc, char* argv[])
         cout << argv[1] << endl;
     #endif
 
-    #define INSTANCE_TEST
+    //#define INSTANCE_TEST
     #ifdef INSTANCE_TEST
         mgr_logic* pLogic = mgr_logic::GetInstance();
         pLogic->Init();
@@ -192,7 +192,7 @@ int main(int argc, char* argv[])
         l.test4();
     #endif
 
-    #define PROTO_TEST
+    //#define PROTO_TEST
     #ifdef PROTO_TEST
         //pluto test begin
         Study s;
@@ -200,10 +200,80 @@ int main(int argc, char* argv[])
         //pluto test end
     #endif
 
-    putchar('\n');
-	system("pause");
-	return 0;
+    //#define NORMAL_END
+    #ifdef NORMAL_END
+        putchar('\n');
+	    system("pause");
+	    return 0;
+    #endif
 
-
+    #define SOCKET_TEST
+    #ifdef SOCKET_TEST
+        //初始化WSA
+        WORD sockVersion = MAKEWORD(2,2);
+        WSADATA wsaData;
+        if(WSAStartup(sockVersion, &wsaData)!=0)
+        {
+            return 0;
+        }
+     
+        //创建套接字
+        SOCKET slisten = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if(slisten == INVALID_SOCKET)
+        {
+            printf("socket error !");
+            return 0;
+        }
+     
+        //绑定IP和端口
+        sockaddr_in sin;
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(8888);
+        sin.sin_addr.S_un.S_addr = INADDR_ANY; 
+        if(bind(slisten, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
+        {
+            printf("bind error !");
+        }
+     
+        //开始监听
+        if(listen(slisten, 5) == SOCKET_ERROR)
+        {
+            printf("listen error !");
+            return 0;
+        }
+     
+        //循环接收数据
+        SOCKET sClient;
+        sockaddr_in remoteAddr;
+        int nAddrlen = sizeof(remoteAddr);
+        char revData[255]; 
+        while (true)
+        {
+            printf("wait link...\n");
+            sClient = accept(slisten, (SOCKADDR *)&remoteAddr, &nAddrlen);
+            if(sClient == INVALID_SOCKET)
+            {
+                printf("accept error !");
+                continue;
+            }
+            printf("acceot a link：%s \r\n", inet_ntoa(remoteAddr.sin_addr));
+             
+            //接收数据
+            int ret = recv(sClient, revData, 255, 0);       
+            if(ret > 0)
+            {
+                revData[ret] = 0x00;
+                printf(revData);
+            }
+     
+            //发送数据
+            const char * sendData = "hello,TCP Client\n";
+            send(sClient, sendData, strlen(sendData), 0);
+            closesocket(sClient);
+        }
+         
+        closesocket(slisten);
+        WSACleanup();
+    #endif
 }
 
--- ghost.h
+++ ghost.h
@@ -3,11 +3,16 @@
 #include "learn.h"
 #include <iostream>
 #include <cstdio>
+#include <stdlib.h>
 #include "pluto.h"
 #include "LogisticRegression.h"
 #include "mgr.h"
 #include <windows.h> 
 #include <process.h> 
+#include <winsock2.h> 
+#pragma comment(lib,"ws2_32.lib") 
+//#pragma comment(lib,"wsock32.lib") 
+
 using namespace std;
 
 template <class T,class U> T GetMin (T a, U b) { return (a<b?a:b); }
